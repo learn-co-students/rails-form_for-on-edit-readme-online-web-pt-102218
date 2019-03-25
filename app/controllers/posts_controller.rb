53c08d2c59ab5a1bbf963ac00e197bdd722f4637
grant smith <grantsmith1256@gmail.com>
2019-03-25 20:09:36 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
-	  redirect_to post_path(@post)
+	  redirect_to edit_post_path(@post)
 	end
 
 	def edit
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(title: params[:post][:title], description: params[:post][:description])
 	  redirect_to post_path(@post)
 	end
 end
